b0a5ace0572f59dd2cdcbe9f2760f0b042802e18
xiaokai <kexiaokai@gmail.com>
2016-04-06 15:43:35 +0800

    [UPD] watch props

--- src/Qrcode.vue
+++ src/Qrcode.vue
@@ -22,31 +22,53 @@ export default {
     bgColor: String,
     fgColor: String
   },
+  watch: {
+    size: function(){
+      this.update()
+    },
+    val: function(){
+      this.update()
+    },
+    level: function(){
+      this.update()
+    },
+    bgColor: function(){
+      this.update()
+    },
+    fgColor: function(){
+      this.update()
+    }
+  },
   ready () {
-    var size = this.size
-    var bgColor = this.bgColor
-    var fgColor = this.fgColor
-    var $qr = this.$els.qr
+    this.update()
+  },
+  methods:{
+    update () {
+      var size = this.size
+      var bgColor = this.bgColor
+      var fgColor = this.fgColor
+      var $qr = this.$els.qr
 
-    var qrcode = qr(this.val)
+      var qrcode = qr(this.val)
 
-    var ctx = $qr.getContext('2d')
-    var cells = qrcode.modules
-    var tileW = size / cells.length
-    var tileH = size / cells.length
-    var scale = (window.devicePixelRatio || 1) / getBackingStorePixelRatio(ctx)
+      var ctx = $qr.getContext('2d')
+      var cells = qrcode.modules
+      var tileW = size / cells.length
+      var tileH = size / cells.length
+      var scale = (window.devicePixelRatio || 1) / getBackingStorePixelRatio(ctx)
 
-    $qr.height = $qr.width = size * scale
-    ctx.scale(scale, scale)
+      $qr.height = $qr.width = size * scale
+      ctx.scale(scale, scale)
 
-    cells.forEach(function (row, rdx) {
-      row.forEach(function (cell, cdx) {
-        ctx.fillStyle = cell ? fgColor : bgColor
-        var w = (Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW))
-        var h = (Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH))
-        ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h)
+      cells.forEach(function (row, rdx) {
+        row.forEach(function (cell, cdx) {
+          ctx.fillStyle = cell ? fgColor : bgColor
+          var w = (Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW))
+          var h = (Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH))
+          ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h)
+        })
       })
-    })
+    }
   }
 }
 
