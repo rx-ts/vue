945494694aea4abdebe4a67d6b78b822b4e8240d
xiaokai <kexiaokai@gmail.com>
2016-11-16 14:49:23 +0800

    style: update code style to es 6

--- src/Qrcode.vue
+++ src/Qrcode.vue
@@ -13,7 +13,7 @@
 
 
 <script>
-import qr from 'qr.js';
+import qr from 'qr.js'
 
 const update = function() {
   this.update();
@@ -44,33 +44,33 @@ export default {
   mounted: update,
   methods: {
     update() {
-      var size = this.size;
-      var bgColor = this.bgColor;
-      var fgColor = this.fgColor;
-      var $qr = this.$refs.qr;
+      var size = this.size
+      var bgColor = this.bgColor
+      var fgColor = this.fgColor
+      var $qr = this.$refs.qr
 
-      var qrcode = qr(this.val);
+      var qrcode = qr(this.val)
 
-      var ctx = $qr.getContext('2d');
-      var cells = qrcode.modules;
-      var tileW = size / cells.length;
-      var tileH = size / cells.length;
-      var scale = (window.devicePixelRatio || 1) / getBackingStorePixelRatio(ctx);
+      var ctx = $qr.getContext('2d')
+      var cells = qrcode.modules
+      var tileW = size / cells.length
+      var tileH = size / cells.length
+      var scale = (window.devicePixelRatio || 1) / getBackingStorePixelRatio(ctx)
 
-      $qr.height = $qr.width = size * scale;
-      ctx.scale(scale, scale);
+      $qr.height = $qr.width = size * scale
+      ctx.scale(scale, scale)
 
-      cells.forEach(function(row, rdx) {
-        row.forEach(function(cell, cdx) {
-          ctx.fillStyle = cell ? fgColor : bgColor;
-          var w = (Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW));
-          var h = (Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH));
-          ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h);
-        });
-      });
+      cells.forEach( (row, rdx) => {
+        row.forEach( (cell, cdx) => {
+          ctx.fillStyle = cell ? fgColor : bgColor
+          var w = (Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW))
+          var h = (Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH))
+          ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h)
+        })
+      })
     }
   }
-};
+}
 
 function getBackingStorePixelRatio(ctx) {
   return (
@@ -80,7 +80,7 @@ function getBackingStorePixelRatio(ctx) {
     ctx.oBackingStorePixelRatio ||
     ctx.backingStorePixelRatio ||
     1
-  );
+  )
 }
 
 </script>
