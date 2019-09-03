9f65720cf4e96a0cada6678ad73e75718c60f19e
JounQin <admin@1stg.me>
2019-09-04 07:45:55 +0800

    docs: generate root CHANGELOG.md file

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+- [vue-qrious](./packages/vue-qrious/CHANGELOG.md)
--- packages/vue-qrious/qrious.d.ts
+++ packages/vue-qrious/qrious.d.ts
@@ -1,23 +1,23 @@
-declare module 'qrious' {
-  namespace QRious {
-    interface QRiousOptions {
-      value: string
-      background?: string
-      backgroundAlpha?: number
-      foreground?: string
-      foregroundAlpha?: number
-      level?: 'L' | 'M' | 'Q' | 'H'
-      mime?: string
-      padding?: number
-      size?: number
-    }
+declare namespace QRious {
+  interface QRiousOptions {
+    value: string
+    background?: string
+    backgroundAlpha?: number
+    foreground?: string
+    foregroundAlpha?: number
+    level?: 'L' | 'M' | 'Q' | 'H'
+    mime?: string
+    padding?: number
+    size?: number
   }
-
-  class QRious {
-    constructor(options?: QRious.QRiousOptions)
-    toDataURL(mime?: string): string
-    set(options?: QRious.QRiousOptions): void
-  }
-
-  export = QRious
 }
+
+declare class QRious {
+  constructor(options?: QRious.QRiousOptions)
+  toDataURL(mime?: string): string
+  set(options?: QRious.QRiousOptions): void
+}
+
+export = QRious
+
+export as namespace QRious
